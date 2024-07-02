2b83879363b83254d0685406754cde3906f280db
anaykhandekar <ayankr0504@gmail.com>
2024-07-01 21:15:42 -0700

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const openai = new OpenAI({
-    apiKey: ""
+    apiKey: "a"
 });
 
 const doData = XLSX.readFile("./src/activity-excel.xlsx");
